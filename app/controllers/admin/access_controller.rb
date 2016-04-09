d5c5336df57da8815e71bf61846a532bc0fa68ec
Smulligan85 <sean.mulligan85@gmail.com>
2016-04-09 13:28:00 -0700

    Done

--- app/controllers/admin/access_controller.rb
+++ app/controllers/admin/access_controller.rb
@@ -1,21 +1,18 @@
 class Admin::AccessController < ApplicationController
   def index
-    @preference = Preference.find_or_create_by(:id => 1)
+    @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true)
   end
 
   def update
     @preference = Preference.find(params[:id])
-    if @preference.update(preference_params)
-      redirect_to admin_access_index_path
-    else
-      render "index"
-    end
+    @preference.update(preference_params)
+    redirect_to admin_access_index_path
   end
 
   private
 
   def preference_params
-    params.require(:preference).permit(:allow_create_artists, :allow_create_songs)
+    params.require(:preference).permit(:allow_create_songs, :allow_create_artists)
   end
 
 end
--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,20 +1,18 @@
 class Admin::SettingsController < ApplicationController
   def index
-    @preference = Preference.find_or_create_by(:id => 1)
+    @preference = Preference.first_or_create(song_sort_order: "DESC", artist_sort_order: "DESC")
   end
 
   def update
     @preference = Preference.find(params[:id])
-    if @preference.update(preference_params)
-      redirect_to admin_settings_index_path
-    else
-      render "index"
-    end
+    @preference.update(preference_params)
+    redirect_to admin_settings_index_path
   end
 
   private
-
+   
   def preference_params
     params.require(:preference).permit(:artist_sort_order, :song_sort_order)
   end
+
 end
